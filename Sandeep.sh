431e7b8fbd6272e5577ee12a0e8daa538fff63ce
Cibbi546 <105946907+Cibbi546@users.noreply.github.com>
2023-07-11 00:41:25 +0530

    Create Sandeep.sh

---
+++ Sandeep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Loop through numbers 1 to 20
+for ((i=1; i<=20; i++))
+do
+  # Check if the number is odd
+  if (( i % 2 != 0 ))
+  then
+    # Print the odd number
+    echo $i
+  fi
+done
+#Display Odd numbers
+file_path="/var/lib/jenkins/workspace/Demogorgan/odd.txt"
+echo "$i to $file_path"
