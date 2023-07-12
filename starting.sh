e67f8a2c2e84d2180db81b9f7639f75d9f4b05ac
Cibbi546 <105946907+Cibbi546@users.noreply.github.com>
2023-07-12 19:49:22 +0530

    Update starting.sh

--- starting.sh
+++ starting.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y git
-sudo yum install -y docker
-sudo service docker start
+sudo su
+yum update -y
+yum install -y git
+yum install -y docker
+service docker start
 chmod 666 /var/run/docker.sock
